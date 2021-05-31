1b5a2d26c7feeaad44754982d64cda99395c98c6
TheRealFury <ayush.abhishek.bhatt@gmail.com>
2021-05-31 10:15:32 +0530

    Added logo and some text.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 var ayushLogo;
+var logo;
 function preLoad(){
   ayushLogo = loadImage("AyushLogo.png");
 }
+function setup(){
+  createCanvas(displayWidth,displayHeight);
+  logo = createSprite(200,200,10,10);
+}
 function draw(){
-  image(ayushLogo,200,200);
+  background(220);
+  drawSprites();
 }
